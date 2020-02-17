92a092ba76b451f58f8474aa7efb29b9d79eb1a2
baboomerang <irodrigue101@gmail.com>
2020-02-17 02:15:51 -0500

    added support for xmp profile saving

--- readspd.sh
+++ readspd.sh
@@ -27,66 +27,91 @@ if [ "$EUID" -ne 0 ]; then
     exit
 fi
 
-usage() {   echo "Usage: $0 [-b busaddr] [-d dimaddr <0x##>] optional:[dimmaddr2] [dimmaddr3] [dimmaddr4]" 1>&2; exit 1; }
+usage() { echo "Usage: $0 [-b busaddr] [-d dimaddr <0x##>] [--xmp profile <1||2>] optional:[dimmaddr2] [dimmaddr3] [dimmaddr4]" 1>&2; exit 1; }
 
 main() {
-    while getopts ":b:d:" o; do
+    while getopts ":b:d:e:" o; do
         case "${o}" in
             b)
-                BUS=$2
+                BUS=${OPTARG}
                 ;;
             d)
-                DIMM=$4
+                DIMM=${OPTARG}
+                ;; 
+            e)
+                echo "test"
+                XMP=0
+                #(( XMP == 1 || XMP == 2 ))  || usage
                 ;;
             *)
                 usage
                 ;;
         esac
     done
-    shift $((OPTIND-1))
+    shift "$((OPTIND-1))"
+
     
     if [ -z "${BUS}" ] || [ -z "${DIMM}" ]; then
         usage
     else
-        readSPD $DIMM 
+        readSPD $DIMM
     fi
     
-    if [[ ! $5 -eq 0 ]]; then
-        DIMM2=$5
+    if [[ ! $1 -eq 0 ]]; then
+        DIMM2=$1
         readSPD $DIMM2
     fi
 
-    if [[ ! $6 -eq 0 ]]; then 
-        DIMM3=$6
+    if [[ ! $2 -eq 0 ]]; then 
+        DIMM3=$2
         readSPD $DIMM3
     fi
 
-    if [[ ! $7 -eq 0 ]]; then
-        DIMM4=$7
+    if [[ ! $3 -eq 0 ]]; then
+        DIMM4=$3
         readSPD $DIMM4
     fi
 
 }
 
-readSPD() {
-    i2cdump ${BUS} ${DIMM}
+#errcheck() { if [$? -ne 0]; then echo "Error, make sure i2c_dev and i2c_i801 modules are loaded. Check if you also have perms for current PWD" 1>&2; exit 1; }
 
+
+readSPD() {
+    i2cdump ${BUS} ${1} b
+    
     if [ $? -ne 0 ]; then
-        echo ""
         echo "Error, make sure i2c_dev and i2c_i801 modules are loaded."
-        echo ""
         exit 1
     fi
 
-    echo "" | tr -d '\n' > dimm${1}.spd 
+    if [[ -n ${XMP} ]]; then
+        EXT="xmp"
+        OFFSET=176
+        END=250
+    else
+        EXT="spd"
+        OFFSET=0
+        END=255
+    fi
+
+    DATE=$(date +%Y-%m-%d)
+    echo "" | tr -d '\n' > dimm${1}.${DATE}.${EXT}
     
-    for INDEX in {0..255}; do 
+    if [ $? -ne 0 ]; then
+        echo "Error writing file. Check file/folder permissions and try again."
+        exit 1
+    fi
+
+    while [ ${OFFSET} -lt ${END} ]; do
+        echo -en "\rReading: $OFFSET/$END"
         HEX=$(i2cget -y ${BUS} ${1} ${INDEX} | sed -ne 's/^0x\(.*\)/\1/p')
-        printf "\x${HEX}" >> dimm${1}.spd
+        printf "\x${HEX}" >> dimm${1}.${DATE}.${EXT}
     done 
+   
     # contents of printf cant be saved to a bash variable, C-string ignores nullbytes 
     # and other special characters. The output must be sent to a file immediately.
-    # Alternatively, echo -e is not posix compliant so printf is the easiest way.     
+    # alternatively, echo -e is not posix compliant so printf is the best way.     
 }
 
 main "$@"
