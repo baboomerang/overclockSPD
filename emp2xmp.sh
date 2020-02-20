3241b1b474d6f2009064c2b5d2b6e5738e133c03
baboomerang <irodrigue101@gmail.com>
2020-02-20 00:48:09 -0500

    fixed emp2xmp and used space delimiters for storing values properly in the array

--- emp2xmp.sh
+++ emp2xmp.sh
@@ -1,7 +1,9 @@
+#!/bin/sh
+
 usage() { echo "Usage: $0 <empfile>" 1>&2; exit 1; }
 
 main() {
-    if [ ! $# = 2 ]; then
+    if [ $# -lt 1 ]; then
         usage
     else
         EMPFILE=$1
@@ -11,63 +13,91 @@ main() {
             usage
         fi
 
+        RAWHEX=$(cat ${EMPFILE} | xxd -p -c 256 | tr -d '\n'| sed -e 's/../& /g')
+        #all of the EMPFILE is stored into the first element of the HEX.
+
+        ARRAYHEX=($RAWHEX)
+        HEXFILELEN=${#ARRAYHEX[@]}
+
+        if [ ${HEXFILELEN} != 52 ]; then
+            echo "EMP File size must be 52 bytes!"
+            exit 1
+        fi
+
         DATE=$(date +%Y-%m-%d)
         CONVERTEDFILE="${1}.${DATE}.xmp"
-        echo "" | tr -d '\n' > ${CONVERTEDFILE}
+        echo "" | tr -d '\n' > "${CONVERTEDFILE}"
 
         if [ $? -ne 0 ]; then
             echo "Error writing new file. Check file/folder permissions and try again"
             exit 1
         fi
 
+        echo "Creating file and writing header"
         printf "\x0c" >> ${CONVERTEDFILE}           #xmp magic number
         printf "\x4a" >> ${CONVERTEDFILE}           #xmp magic number
         printf "\x05" >> ${CONVERTEDFILE}
     # 00   |    00                           00          |      0        0
-# reserved | prfl2 dimmsPerchannel    prfl1 dimmsPerchnl |   profile2 profile1
+ # reserved | prfl2 dimmsPerchannel    prfl1 dimmsPerchnl |   profile2 profile1
         # result: 0x05 =   0000 0101
         # profile 1 enabled
         # 2 dimms per channel
 
-        #printf "\x${EMPFILE[4]}" >> #emp 4&5th bytes are xmp version #
-        printf "\x${EMPFILE[7]}" >> ${CONVERTEDFILE} #xmp profile 1 MTB dividend
-        printf "\x${EMPFILE[8]}" >> ${CONVERTEDFILE} #xmp profile 1 MTB divisor
-        printf "\x00" >> ${CONVERTEDFILE}            #xmp profile 2 MTB dividend
-        printf "\x00" >> ${CONVERTEDFILE}            #xmp profile 2 MTB divisor
-        printf "\x${EMPFILE[37]}" >> ${CONVERTEDFILE}#reserved byte
-        printf "\x${EMPFILE[9]}" >> ${CONVERTEDFILE} #DIMM VOLTAGE
-        printf "\x${EMPFILE[10]}" >> ${CONVERTEDFILE}#tCK minimum
-        printf "\x${EMPFILE[11]}" >> ${CONVERTEDFILE}#tAA minimum
-        printf "\x${EMPFILE[22]}" >> ${CONVERTEDFILE}
-        printf "\x${EMPFILE[23]}" >> ${CONVERTEDFILE}
-        printf "\x${EMPFILE[12]}" >> ${CONVERTEDFILE}#tcWL minimum
-        printf "\x${EMPFILE[13]}" >> ${CONVERTEDFILE}#tRP  minimum
-        printf "\x${EMPFILE[14]}" >> ${CONVERTEDFILE}#tRCD minimum
-        printf "\x${EMPFILE[15]}" >> ${CONVERTEDFILE}#tWR  minimum
-#19th byte checksum
-        HEXBYTE=$(cat ${EMPFILE[24]} | xxd -p -c 256 | sed -ne 's/./&/p' | tr -d '\n')
+        echo "${ARRAYHEX[@]}"
+        XMPMAJORVERSION=${ARRAYHEX[4]}
+        XMPMAJORVERSION=${XMPMAJORVERSION:1:1}
 
-        printf "\x${EMPFILE[24]}" >> ${CONVERTEDFILE}#tRAS minimum
-        printf "\x${EMPFILE[26]}" >> ${CONVERTEDFILE}#tRC  minimum
-        printf "\x${EMPFILE[28]}" >> ${CONVERTEDFILE}#tREFI max   lower byte
-        printf "\x${EMPFILE[29]}" >> ${CONVERTEDFILE}#tREFI max   upper byte
-        printf "\x${EMPFILE[30]}" >> ${CONVERTEDFILE}#tRFC min    lower byte
-        printf "\x${EMPFILE[31]}" >> ${CONVERTEDFILE}#tRFC min    upper byte
-        printf "\x${EMPFILE[34]}" >> ${CONVERTEDFILE}#tRTP min
-        printf "\x${EMPFILE[16]}" >> ${CONVERTEDFILE}#tRRD min
-        printf "\x00" >> ${CONVERTEDFILE}            #28th byte skipped -ZEROED
-        printf "\x${EMPFILE[32]}" >> ${CONVERTEDFILE}#tFAW min upper nibble
-        printf "\x${EMPFILE[17]}" >> ${CONVERTEDFILE}#tFAW min byte
-        printf "\x00" >> ${CONVERTEDFILE}            #31st byte skipped -ZEROED
-        printf "\x00" >> ${CONVERTEDFILE}            #32nd byte skipped -ZEROED
-        printf "\x${EMPFILE[21]}" >> ${CONVERTEDFILE}#tWTR min
-        printf "\x00" >> ${CONVERTEDFILE}            #34th byte skipped -ZEROED
-        printf "\x${EMPFILE[36]}" >> ${CONVERTEDFILE}
-        printf "\x${EMPFILE[38]}" >> ${CONVERTEDFILE}
-        printf "\x${EMPFILE[39]}" >> ${CONVERTEDFILE}
-        printf "\x${EMPFILE[40]}" >> ${CONVERTEDFILE}
-        printf "\x${EMPFILE[41]}" >> ${CONVERTEDFILE}
-        printf "\x${EMPFILE[42]}" >> ${CONVERTEDFILE}
+        XMPMINORVERSION=${ARRAYHEX[5]}
+        XMPMINORVERSION=${XMPMINORVERSION:1:1}
+
+        printf "\x${XMPMAJORVERSION}${XMPMINORVERSION}" >> ${CONVERTEDFILE}
+
+        printf "\x${ARRAYHEX[7]}" >> ${CONVERTEDFILE}     #xmp profile 1 MTB dividend
+        printf "\x${ARRAYHEX[8]}" >> ${CONVERTEDFILE}     #xmp profile 1 MTB divisor
+        printf "\x00" >> ${CONVERTEDFILE}                 #xmp profile 2 MTB dividend
+        printf "\x00" >> ${CONVERTEDFILE}                 #xmp profile 2 MTB divisor
+        printf "\x${ARRAYHEX[37]}" >> ${CONVERTEDFILE}    #reserved byte
+        printf "\x${ARRAYHEX[9]}" >> ${CONVERTEDFILE}     #DIMM VOLTAGE
+        printf "\x${ARRAYHEX[10]}" >> ${CONVERTEDFILE}    #tCK minimum
+        printf "\x${ARRAYHEX[11]}" >> ${CONVERTEDFILE}    #tAA minimum
+        printf "\x${ARRAYHEX[22]}" >> ${CONVERTEDFILE}    
+        printf "\x${ARRAYHEX[23]}" >> ${CONVERTEDFILE}    
+        printf "\x${ARRAYHEX[12]}" >> ${CONVERTEDFILE}    #tcWL minimum
+        printf "\x${ARRAYHEX[13]}" >> ${CONVERTEDFILE}    #tRP  minimum
+        printf "\x${ARRAYHEX[14]}" >> ${CONVERTEDFILE}    #tRCD minimum
+        printf "\x${ARRAYHEX[15]}" >> ${CONVERTEDFILE}    #tWR  minimum
+
+        UPPERtRASNIBBLE=${ARRAYHEX[24]}                   #this is the full byte
+        UPPERtRASNIBBLE=${UPPERtRASNIBBLE:1:1}
+
+        UPPERtRCNIBBLE=${ARRAYHEX[26]}                    #this is the full byte
+        UPPERtRCNIBBLE=${UPPERtRCNIBBLE:1:1}
+
+        printf "\x${UPPERtRASNIBBLE}${UPPERtRCNIBBLE}" >> ${CONVERTEDFILE}
+
+        printf "\x${ARRAYHEX[24]}" >> ${CONVERTEDFILE}    #tRAS minimum
+        printf "\x${ARRAYHEX[26]}" >> ${CONVERTEDFILE}    #tRC  minimum
+        printf "\x${ARRAYHEX[28]}" >> ${CONVERTEDFILE}    #tREFI max   lower byte
+        printf "\x${ARRAYHEX[29]}" >> ${CONVERTEDFILE}    #tREFI max   upper byte
+        printf "\x${ARRAYHEX[30]}" >> ${CONVERTEDFILE}    #tRFC min    lower byte
+        printf "\x${ARRAYHEX[31]}" >> ${CONVERTEDFILE}    #tRFC min    upper byte
+        printf "\x${ARRAYHEX[34]}" >> ${CONVERTEDFILE}    #tRTP min
+        printf "\x${ARRAYHEX[16]}" >> ${CONVERTEDFILE}    #tRRD min
+        printf "\x00" >> ${CONVERTEDFILE}                 #28th byte skipped -ZEROED
+        printf "\x${ARRAYHEX[32]}" >> ${CONVERTEDFILE}    #tFAW min upper nibble
+        printf "\x${ARRAYHEX[17]}" >> ${CONVERTEDFILE}    #tFAW min byte
+        printf "\x00" >> ${CONVERTEDFILE}                 #31st byte skipped -ZEROED
+        printf "\x00" >> ${CONVERTEDFILE}                 #32nd byte skipped -ZEROED
+        printf "\x${ARRAYHEX[21]}" >> ${CONVERTEDFILE}    #tWTR min
+        printf "\x00" >> ${CONVERTEDFILE}                 #34th byte skipped -ZEROED
+        printf "\x${ARRAYHEX[36]}" >> ${CONVERTEDFILE}
+        printf "\x${ARRAYHEX[38]}" >> ${CONVERTEDFILE}
+        printf "\x${ARRAYHEX[39]}" >> ${CONVERTEDFILE}
+        printf "\x${ARRAYHEX[40]}" >> ${CONVERTEDFILE}
+        printf "\x${ARRAYHEX[41]}" >> ${CONVERTEDFILE}
+        printf "\x${ARRAYHEX[42]}" >> ${CONVERTEDFILE}
+        echo "finished"
     fi
-
 }
+
+main "$@"
