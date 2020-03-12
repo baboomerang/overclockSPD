54cbc2adf9b2028f95bf58953fe26343e0664fe1
baboomerang <irodrigue101@gmail.com>
2020-03-12 14:30:31 -0400

    readspd properly asks user to confirm the disclaimer and reads the bytes

--- readspd.py
+++ readspd.py
@@ -40,14 +40,17 @@ def readspd(busaddr, dimmaddr):
         print('Cannot write dump to current directory')
         sys.exit(1)
     else:
-        print('WARNING! This program may confuse your i2c bus.')
-        ans = input('continue? (y/N) << ').lower()
+        print('WARNING! This program can confuse your I2C bus, cause data loss and worse!')
+        print('I will read from device file /dev/i2c', busaddr, 'chip address', dimmaddr, \
+              'current data address, using read byte')
+
+        ans = input('continue? (yes/y/N/No) << ').lower()
         if ans in ['yes', 'y']:
             for index in range(0, 255):
-                i2cproc = subprocess.Popen(['i2cget', str(busaddr), str(dimmaddr), \
+                i2cproc = subprocess.Popen(['i2cget', '-y', str(busaddr), str(dimmaddr), \
                         str(index)], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
                 output, err = i2cproc.communicate()
-                print(output)
+                print(output,err)
         else:
             print('User did not type yes/y/Y. No changes have been made. Exiting')
             sys.exit(1)
