69e5f6eb092f80bc3aa5adcd453c771e35c2a987
baboomerang <irodrigue101@gmail.com>
2020-03-12 17:29:37 -0400

    writespd properly prompts user and warns them of risks, renamed writespd function parameters

--- writespd.py
+++ writespd.py
@@ -17,6 +17,7 @@ def main(argv):
     if os.getuid():
         print("Please run as root.")
         sys.exit(1)
+
     try:
         opts, argv = getopt.getopt(argv, "hb:d:f:", ["bus=", "dimm=", "filepath="])
     except getopt.error:
@@ -36,22 +37,30 @@ def main(argv):
 
     writespd(bus, dimm, inputpath)
 
-def writespd(bus, dimm, filepath):
+def writespd(busaddr, dimmaddr, filepath):
     spdfile = Path(str(filepath))
 
     if not spdfile.is_file():
         print('Input file not found.')
         sys.exit(1)
     else:
-        spdfile = open(spdfile, "rb")
-        for _ in range(0, 255):
-            byte = spdfile.read(1)
-            spdfile.seek(1)
-            print(byte)
-            i2cset_proc = subprocess.Popen(['i2cset', bus, dimm, byte], \
-                    stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-            output, err = i2cset_proc.communicate()
+        print('WARNING! This program can confuse your I2C bus, cause data loss and worse!')
+        print('I will write to device file /dev/i2c-',busaddr, 'chip address', dimmaddr, \
+              ', using write byte')
 
+        ans = input('continue? (yes/y/N/No) << ').lower()
+        if ans in ['yes', 'y']:
+            spdfile = open(spdfile, "rb")
+            for _ in range(0, 255):
+                byte = spdfile.read(1)
+                spdfile.seek(1)
+                print(byte)
+                i2cset_proc = subprocess.Popen(['i2cset', '-y', bus, dimm, byte], \
+                        stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+                output, err = i2cset_proc.communicate()
+        else:
+            print('User did not type yes/y/Y. No changes have been made. Exiting')
+            sys.exit(1)
 
 if __name__ == "__main__":
     main(sys.argv[1:])
