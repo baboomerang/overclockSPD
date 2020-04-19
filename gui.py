ea2111d52daec1566c40cd27e43e0fecab95eb04
baboomerang <irodrigue101@gmail.com>
2020-04-19 17:36:28 -0400

    new boilerplate gui code inserted

--- gui.py
+++ gui.py
@@ -1,23 +1,66 @@
 import curses
+from curses import wrapper
 
-stdscr = curses.initscr()
-sh, sw = stdscr.getmaxyx()
+s = curses.initscr()
+sh, sw = s.getmaxyx()
 curses.noecho()
-curses.cbreak()          #implicitly called by default, redeclaring for the user
+curses.cbreak()
 
-def main(stdscr):
-    spdmenu = curses.newwin(sh, sw, 0, 0)
-    spdmenu.keypad(True)
+menu = ['Edit SPD/XMP dump', 'Read SPD/XMP from RAM', 'Write SPD/XMP to RAM']
+
+def print_menu(scr, key_value):
+    scr.clear()
+
+    for row_index, row in enumerate(menu):
+        x = sw//2 - len(row)//2
+        y = sh//2 - len(menu)//2 + row_index
+
+        if row_index == key_value:
+            scr.attron(curses.color_pair(1))
+            scr.addstr(y, x, row)
+            scr.attroff(curses.color_pair(1))
+        else:
+            scr.addstr(y, x, row)
+
+    scr.refresh()
+
+
+def main(s):
     curses.curs_set(0)
-    spdmenu.addstrt(sh, 0, "Hello world!")
+    curses.init_pair(1, curses.COLOR_BLACK, curses.COLOR_WHITE)
+
+    w = curses.newwin(sh, sw, 0, 0)
+    w.keypad(True)
+    w.attron(curses.color_pair(1))
+
+    key = 0
+    index = 0
+    print_menu(w, index)
 
     while True:
-        spdmenu.refresh()
-        key = spdmenu.getch()
+        key = w.getch()
+        w.clear()
+        if key == curses.KEY_LEFT and index > 0:
+            index -= 1;
+        elif key == curses.KEY_RIGHT and index < (len(menu) - 1):
+            index += 1;
+        elif key == curses.KEY_ENTER:
+            w.clear()
+            w.addstr(0, 0, "You pressed {}".format(menu[index]))
+            w.refresh()
+            w.getch()
 
-def curseexit():
+        print_menu(w, index)
+        w.refresh()
+
+
+    curses.nocbreak()
+    s.keypad(False)
+    curses.echo()
     curses.endwin()
-    quit()
 
+wrapper(main) #used for debug purposes only
 
-wrapper(main)
+#if __name__ == "__main__":
+#        main(s)
+
