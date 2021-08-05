6859e5bf7dca76aa6e55564336084352793495e0
baboomerang <irodrigue101@gmail.com>
2021-08-05 01:02:45 -0400

    General refactor, the code is cleaner and better
    Updated Usage() functions so they are more helpful for a new user.
    Reviewed and removed unnecessary comments, global variables, and typos.

--- readspd.sh
+++ readspd.sh
@@ -6,7 +6,7 @@
 
 # READSPD.sh ===============================================================
 # Make sure i2c_dev, i2c_i801, i2c_algo_bit are loaded.
-# This should read the spd data of the RAM (255 bytes) and write it to a file named 
+# This should read the spd data of the RAM (255 bytes) and write it to a file named
 # dimm{hex-address}.spd in the current PWD
 
 #<<COMMENT
@@ -22,100 +22,83 @@
 #you may suffer in connection with using, modifying, or distributing this "readspd" script.
 #COMMENT
 
-#if [ "$EUID" -ne 0 ]; then    #bashism
-
-if [ "$(id -u)" -ne 0 ]; then
-    echo "Please run as root"
-    exit
-fi
-
-usage() { echo "Usage: $0 [-x]xmponly [-b <0-9>] [-d dimaddr <0x##>]" 1>&2; exit 1; }
-
-main() {
-    while getopts "b:d:x" o; do
-        case "${o}" in
-            b)
-                BUS=${OPTARG}
-                ;;
-            d)
-                DIMM=${OPTARG}
-                ;; 
-            x)
-                XMP=1
-                ;;
-            *)
-                usage
-                ;;
-        esac
-    done
-    shift "$((OPTIND-1))"
- 
-    if [ -z ${BUS} ] || [ -z ${DIMM} ]; then
-        usage
-    else
-        readSPD ${DIMM}
-    fi
-    
-    #removed to keep consistent script behavior between the python version and the bash version
-    #if [ -n "$1" ]; then
-    #    DIMM2=$1
-    #    readSPD ${DIMM2}
-    #fi
-
-    #if [ -n "$2" ]; then 
-    #    DIMM3=$2
-    #    readSPD ${DIMM3}
-    #fi
-
-    #if [ -n "$3" ]; then
-    #    DIMM4=$3
-    #    readSPD ${DIMM4}
-    #fi
-
+usage() {
+    echo "Usage: $0 [-x]XMP ONLY [-b] I2C BUS ADDRESS [-d] DIMM ADDRESS" 1>&2;
+    echo "I2C BUS ADDRESS is an integer (usually 0-9)" 1>&2;
+    echo "DIMM ADDRESS is an integer (usually 0x50 through 0x54 for DDR3 systems)" 1>%2;
+    echo "Example: $0 -b 6 -d 0x50" 1>&2;
+    exit 1;
 }
 
-#errcheck() { if [$? -ne 0]; then echo "Error, make sure i2c_dev and i2c_i801 modules are loaded. Check if you also have perms for current PWD" 1>&2; exit 1; }
+main() {
+    # Script must be ran as root user
+    if [ "$(id -u)" -ne 0 ]; then
+        echo "Please run as root"
+        exit 1
+    fi
+
+    # Get and parse commandline options
+    while getopts "b:d:x" o; do
+        case "${o}" in
+            b)  BUS=${OPTARG};;
+            d)  DIMM=${OPTARG};;
+            x)  XMP_MODE=1;;
+            *)  usage;;
+        esac
+    done
+
+    # Shift out all parameters except the last one (the input file)
+    shift "$((OPTIND-1))"
+
+    # Script can only work with valid BUS and DIMM numbers
+    if [ -z "$BUS" ] || [ -z "$DIMM" ]; then
+        usage
+    fi
+
+    readSPD
+
+    exit 0
+}
 
 readSPD() {
-    i2cdump ${BUS} ${1} b
-    
-    if [ $? -ne 0 ]; then
+    # Print SPD contents to terminal in bytes
+    if [ $(i2cdump "$BUS" "$DIMM" b) ]; then
         echo "Error, make sure i2c_dev and i2c_i801 modules are loaded."
         exit 1
     fi
 
-    if [ -n "$XMP" ]; then
-        EXT="xmp"
-        INDEX=176
-        END=250
+    # File Extension, Byte offset into SPD region, Byte End
+    local ext=""
+    local index=0
+    local end=0
+
+    if [ -n "$XMP_MODE" ]; then
+        ext="xmp"
+        index=176
+        end=250
     else
-        EXT="spd"
-        INDEX=0
-        END=255
+        ext="spd"
+        index=0
+        end=255
     fi
 
-    DATE=$(date +%Y-%m-%d)
-    echo "" | tr -d '\n' > dimm"${1}"."${DATE}".${EXT}
-    
+    local date=$(date +%Y-%m-%d)
+    echo "" | tr -d '\n' > dimm"$DIMM"."$date".$ext
+
     if [ $? -ne 0 ]; then
         echo "Error writing file. Check file/folder permissions and try again."
         exit 1
     fi
 
-    while [ ${INDEX} -le ${END} ]; do
-        echo "Reading from SPD: $INDEX/$END"
-        HEX=$(i2cget -y ${BUS} ${1} ${INDEX} | sed -ne 's/^0x\(.*\)/\1/p')
-        echo "${HEX}"
-        printf "\x${HEX}" >> dimm"${1}"."${DATE}".${EXT}
-        INDEX=$((INDEX+1))
-    done 
-    
-    echo ""
-    echo "Dump written to: ${PWD}/dimm${1}.${DATE}.${EXT}"
-   
-    # contents of printf cant be saved to a bash variable, C-string ignores nullbytes 
-    # and other special characters. The output must be sent to a file immediately.
-    # alternatively, echo -e is not posix compliant so printf is the best way.     
+    while [ ${index} -le ${end} ]; do
+        echo "Reading from SPD: $index/$end"
+        local hex=$(i2cget -y "$BUS" "$DIMM" ${index} | sed -ne 's/^0x\(.*\)/\1/p')
+        echo "${hex}"
+        printf "\x${hex}" >> dimm"$DIMM"."$date".$ext
+        index=$((index+1))
+    done
+
+    echo "\nDump written to: ${PWD}/dimm$DIMM.$DATE.$EXT"
 }
 
 main "$@"
--- writespd.sh
+++ writespd.sh
@@ -22,104 +22,109 @@
 #you may suffer in connection with using, modifying, or distributing this "writespd" script.
 #COMMENT
 
-if [ "$(id -u)" -ne 0 ];
-    then echo "Please run as root"
-    exit
-fi
 
-usage() { echo "Usage: $0 [-x]xmponly [-b busaddr#] [-d dimaddr <0x##>] [FILE]" 1>&2; exit 1; }
+usage() {
+    echo "Usage: $0 [-x]XMP ONLY [-b] I2C BUS ADDRESS [-d] DIMM ADDRESS [FILENAME]" 1>&2;
+    echo "I2C BUS ADDRESS is an integer (usually 0-9)" 1>&2;
+    echo "DIMM ADDRESS is an integer (usually 0x50 through 0x54 for DDR3 systems)" 1>%2;
+    echo "Example: $0 -b 6 -d 0x50 sampledump.spd" 1>&2;
+    exit 1;
+}
 
 main() {
+    # Script must be ran as root user
+    if [ "$(id -u)" -ne 0 ]; then
+        echo "Please run as root"
+        exit 1
+    fi
+
+    # Get and parse commandline options
     while getopts "b:d:x" o; do
         case "${o}" in
-            b)
-                BUS=${OPTARG}
-                ;;
-            d)
-                DIMM=${OPTARG}
-                ;; 
-            x)
-                XMP=1
-                ;;
-            *)
-                usage
-                ;;
+            b)  BUS=${OPTARG};;
+            d)  DIM=${OPTARG};;
+            x)  XMP_MODE=1;;
+            *)  usage;;
         esac
     done
+
+    # Shift out all parameters except the last one (the input file)
     shift "$((OPTIND-1))"
 
     INPUTFILE=$1
 
-    if [ -z "${BUS}" ] || [ -z "${DIMM}" ] || [ -z "${INPUTFILE}" ]; then
+    if [ -z "$BUS" ] || [ -z "$DIMM" ] || [ -z "$INPUTFILE" ]; then
         usage
-    else                                                                                                                          
-        echo "WARNING! Do not write incorrect or bad address for bus/dimm!"
-        echo "Writing to non-dimm locations can cause damage!"
-        echo "Please have backups incase something goes wrong."
-        sleep 1
-     
-        i2cdump -y ${BUS} ${DIMM} b
-
-        if [ $? -ne 0 ]; then
-            echo "Error, make sure i2c_dev and i2c_i801 modules are loaded."
-            exit 1
-        fi
-
-        echo "Preview of target device: is this the device?" 
-        sleep 1
-    
-        read -p "Knowing these risks, you will write to bus ${BUS} address ${DIMM}. Proceed? [yesiwanttoproceedknowingtheserisks/no]: " yn
-        case $yn in
-            yes ) writeSPD DIMM;;
-            #            yesiwanttoproceedknowingtheserisks ) writeSPD DIMM;;
-            no ) exit 1;;
-            * ) echo "Error: response must be exact option, verbatim as given."; exit 1;;
-        esac
     fi
 
+    echo "WARNING! Do not write an incorrect or bad address for i2cbus or dimm!"
+    echo "Writing to non-dimm locations can cause permanent damage!"
+    echo "Please have backups incase something goes wrong."
+    sleep 1
+
+    # Show preview of the target device over i2c (in bytes)
+    if [ $(i2cdump -y "$BUS" "$DIMM" b) ]; then
+        echo "Error, make sure i2c_dev and i2c_i801 modules are loaded."
+        exit 1
+    fi
+    echo "Preview of target device: is this the intended device?"
+    sleep 1
+
+    # Prompt user again as an extra safety precaution
+    read -p "Knowing these risks, you will write to bus $BUS address $DIMM. Proceed? [yes/No]: " yn
+    case $yn in
+        yes ) write_spd;;
+        no ) exit 0;;
+        * ) echo "Error: response must be exactly \"yes\" or \"no\""; exit 1;;
+    esac
+
+    exit 0
 }
 
-writeSPD() {
-    if [ ! -f $INPUTFILE ]; then
+write_spd() {
+    # Check if the file is actually a file and not a directory or something else
+    if [ ! -f "$INPUTFILE" ]; then
         echo "Cannot find file. Double check permissions or location and try again."
         exit 1
     fi
 
-    #these 3 lines may not be posix compliant
-    RAWHEX=$(cat ${INPUTFILE} | xxd -p -c 256 | tr -d '\n'| sed -e 's/../0x& /g')
-    ARRAYHEX=($RAWHEX)
-    FILELENGTH=${#ARRAYHEX[@]}
+    # I used this instead of IFS
+    local rawhex=$(cat "$INPUTFILE" | xxd -p -c 256 | tr -d '\n'| sed -e 's/../0x& /g')
+    local arrayhex=($rawhex)
+    local filelength=${#arrayhex[@]}
 
-    if [ -n "${XMP}" ]; then
-        if [ ${FILELENGTH} != 40 ]; then
+    # File Extension, Byte offset into SPD region, Byte End
+    local ext=""
+    local offset=0
+    local end=0
+
+    if [ -n "$XMP_MODE" ]; then
+        if [ "$filelength" != 40 ]; then
             echo "XMP file must be exactly 40 bytes"
             exit 1
         fi
-        EXT="xmp"
-        OFFSET=176
-        END=219
-        #END=250 #this may erase profile 2 if you have anything there.
+        ext="xmp"
+        offset=176
+        end=219     #XMP Profile 2 starts at the 221st byte (index 220)
     else
-        if [ ${FILELENGTH} != 256 ]; then
+        if [ "$filelength" != 256 ]; then
             echo "SPD file must be exactly 256 bytes"
             exit 1
-        fi    
-        EXT="spd"
-        OFFSET=0;
-        END=255
+        fi
+        ext="spd"
+        offset=0;
+        end=255
     fi
 
-    INDEX=0
-    while [ $((INDEX+OFFSET)) -le ${END} ]; do
-        echo "Writing to SPD: $((INDEX+OFFSET))/${END} BYTE:(${ARRAYHEX[${INDEX}]})"
-        sleep 0.1
-        i2cset -y ${BUS} ${DIMM} $((INDEX+OFFSET)) ${ARRAYHEX[${INDEX}]}
-        INDEX=$((INDEX+1))
+    local index=0
+    while [ $((index+offset)) -le ${end} ]; do
+        echo "Writing to SPD: $((index+offset))/$end BYTE:(${arrayhex[${index}]})"
+        sleep 0.2
+        i2cset -y "$BUS" "$DIMM" $((index+offset)) "${arrayhex[${index}]}"
+        index=$((index+1))
     done
 
-    echo ""
-    i2cdump -y $BUS $DIMM b
-    echo "${INPUTFILE} written successfully to ${DIMM}"
+    echo "\n$INPUTFILE written successfully to $DIMM"
 }
 
 main "$@"
